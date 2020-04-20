8ba31b0860a94d23f582d0cfc0213a7f32e9582e
godsh0t <32084209+godsh0t@users.noreply.github.com>
2020-04-20 15:43:12 -0400

    Add files via upload

---
+++ _stories/story_y7-4_badend.lua
@@ -0,0 +1,21 @@
+BadEnd = Story:extend()
+
+function BadEnd:new()
+    BadEnd.super.new(self)
+    self.lines = { 
+		"*BEEP*",
+		"*BEEP*",
+
+		"SUBJECT: !!!!! A.C.P.P. RESULTS !!!!!",
+		"BODY:",
+		"TAKER.",
+		"WHAT HAVE YOU DONE?",
+		"YOU HAVE FAILED TO PROPERLY CARE FOR YOUR GIVER UNIT.",
+		"YOUR REARING PERIOD WILL BE CUT SHORT,",
+		"AND YOUR PLANET DETONATED.",
+		"EFFECTIVE IMMEDIATELY.",
+		"THANK YOU FOR PARTICIPATING.",
+		"GOODBYE.",
+		"BAD END."
+    }
+end
