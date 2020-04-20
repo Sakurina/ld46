b0a8e11218c37420e485248f828a595f4803dedc
godsh0t <32084209+godsh0t@users.noreply.github.com>
2020-04-19 22:06:10 -0400

    Update story_y2-1_upgrade.lua

--- _stories/story_y2-1_upgrade.lua
+++ _stories/story_y2-1_upgrade.lua
@@ -3,7 +3,41 @@ UpgradeStory = Story:extend()
 function UpgradeStory:new()
     UpgradeStory.super.new(self)
     self.lines = { 
-        "#portrait gfx/portrait_baby_cool.png",
-        "{1} has evolved!"
+        "3015.UC",
+        "01.01 0900",
+        "{player}: \"Good morning, {girl}.\"",
+        "{girl}: ...",
+        "{girl}: \"Good morning..TAKER.\"",
+        "With the dawn, came a new year and with it a reminder of what was to come.",
+        "5 more years, huh...",
+        "I've gotten used to {girl} and her presence in and around the house.",
+        "But man...",
+        "I'll never get used to the TAKER title.",
+        "{player}: \"Hey, could you.. possibly call me something else?\"",
+        "{girl} tilts her head with an inquisitive expression.",
+        "{girl}: \"Like what?\"",
+        "{player}: \"Oh you know, like anything else.\"",
+        "She raises her hand to her chin, clearly deep in thought.",
+        "{girl}: ...",
+        "{girl}: \"..Daddy?\"",
+        "No.",
+        "Absolutely not.",
+        "That's way, way worse.",
+        "{player}: \"Actually you know what, TAKER is fine.\"",
+        "{girl}: \"The name clearly seems to bother you though...\"",
+        "{girl}: \"How about {player}?\"",
+        "Yes, that's me, I'm {player}.",
+        "{player}: \"Truth be told, that's still a bit awkward.\"",
+        "{girl}: \"?\"",
+        "{player}: \"Well, it's just that names are usually reserved for use when you're close with one another.\"",
+        "{girl}: \"We live in the same house.\"",
+        "{player}: \"That is certainly true--\"",
+        "{girl}: \"Is this not close?\"",
+        "She shortens the distance between us.",
+        "I can see the top of her head even while she's looking up at me.",
+        "{player}: \"Names are--\"",
+        "{girl}: \"No.\"",
+        "{girl}: \"This is fine..{player}.\"",
+        "Yes, that's me, I'm {player}."
     }
 end
