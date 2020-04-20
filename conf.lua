7ddbb25d67ea79465d168c8c13563f585236ecc0
Yanik Magnan <kirbykirbykirby@gmail.com>
2020-04-20 12:38:28 -0400

    clean up the config file

--- conf.lua
+++ conf.lua
@@ -1,6 +1,8 @@
 function love.conf(t)
+    t.identity = "machine_learning_ld46"
     t.version = "11.3"
     t.window.width = 1280
     t.window.height = 720
     t.window.highdpi = true
+    t.window.title = "[M]achine [L]earning"
 end
