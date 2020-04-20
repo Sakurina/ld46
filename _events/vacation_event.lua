10191249a5f5f5a3aff266c28ab332a88c9a2b2a
godsh0t <32084209+godsh0t@users.noreply.github.com>
2020-04-20 19:10:35 -0400

    Update vacation_event.lua

--- _events/vacation_event.lua
+++ _events/vacation_event.lua
@@ -5,7 +5,7 @@ function VacationEvent:new()
 
     self.event_name = constants.vacation_event.id
     self.event_success_text = ""
-    self.event_critical_text = "{girl} played volleyball on the beach today and feels great!"
+    self.event_critical_text = "{girl} felt more relaxed than usual in the bath! We should come here every week!"
     self.stat_growths = {
         StatGrowth("money", constants.vacation_event.cost_per_day, constants.vacation_event.cost_per_day),
         StatGrowth("stress", constants.vacation_event.stress_delta_regular, constants.vacation_event.stress_delta_lucky)
