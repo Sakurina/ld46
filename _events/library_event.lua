8c75506dd46ea777039ff09cbf3f3af2cfef42cd
godsh0t <32084209+godsh0t@users.noreply.github.com>
2020-04-20 19:09:19 -0400

    Update library_event.lua

--- _events/library_event.lua
+++ _events/library_event.lua
@@ -5,7 +5,7 @@ function LibraryEvent:new()
     
     self.event_name = constants.library_event.id
     self.event_success_text = ""
-    self.event_critical_text = "{girl} read philosophy books at the library today. How wise!"
+    self.event_critical_text = "{girl} had a breakthrough mentally battling herself. Oh the plight of man!"
     self.stat_growths = {
         StatGrowth("money", constants.library_event.cost_per_day, constants.library_event.cost_per_day),
         StatGrowth("morality", constants.library_event.morality_delta_regular, constants.library_event.morality_delta_lucky)
