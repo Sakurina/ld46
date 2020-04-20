7700de0cf937df89f09b9b561cae69061a2fe471
godsh0t <32084209+godsh0t@users.noreply.github.com>
2020-04-20 19:06:13 -0400

    Update cook_event.lua

--- _events/cook_event.lua
+++ _events/cook_event.lua
@@ -5,7 +5,7 @@ function CookEvent:new()
 
     self.event_name = constants.cook_event.id
     self.event_success_text = ""
-    self.event_critical_text = "{girl}'s cooking today smelled so good."
+    self.event_critical_text = "{girl}'s knife skills were impeccable today. C'est magnifique!"
     self.stat_growths = {
         StatGrowth("dex", constants.cook_event.dex_delta_regular, constants.cook_event.dex_delta_lucky),
         StatGrowth("cooking", constants.cook_event.cooking_delta_regular, constants.cook_event.cooking_delta_lucky)
